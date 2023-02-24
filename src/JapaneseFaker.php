69b28cb725d04a5ce8d4b8b246b09c2ee1fbe6ca
thiago <thiago.tanaka.dev@gmail.com>
2023-02-25 01:43:05 +0900

    first commit

---
+++ src/JapaneseFaker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Robsonrk\JapaneseFaker;
+
+class JapaneseFaker
+{
+    public function __construct()
+    {
+        $this->address = new Address;
+        $this->person = new Person;
+        $this->company = new Company;
+    }
+}
